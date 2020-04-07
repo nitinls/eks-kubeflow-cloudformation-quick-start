46ea062d9fa4bfe8e27c1c5ab5802ffde8bbb2e5
Amit Kalawat <amitkalawat@tests-mbp.lan>
2020-04-06 22:39:09 -0400

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# eks-kubeflow-cloudformation-quick-startv1
+# eks-kubeflow-cloudformation-quick-start
+
+Currently runs only in Ohio. CF will be modified to support more regions in near future. 
+The scripts used in this repo are referenced from https://eksworkshop.com and https://sagemaker.readthedocs.io/en/stable/amazon_sagemaker_operators_for_kubernetes.html
+
+Steps#
+
+1) Create a new Key-pair in AWS Console. 
+2) Execute the Cloudformation template cfv1
+3) The cfv1 will run for about 30 minutes and will setup a Jump Box/Bastion Host VM with eksctl and kubectl , setup EKS, KubeFlow and Sagemaker operators for k8s.
+
+
+
+#Draft Deletion/Roll-Back steps-
+
+1) eksctl delete cluster (eksworkshop-eksctlv10)
+2) Delete IAM OIDC
+3) Delete IAM Role (eksworkshopv10)
+4) Delete ALB 
+5) Delete ALB Target Group.
+6) Delete the EC2 Key pair
+7) Delete/Disable the AWS KMS Custom Key (optional)
+8) Delete the Cloudformations (if there are errors)
+9) Delete the main Cloudformation which stood up VM.
---
+++ scriptv1.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Install EKS
+# Create Cloud9 workspace with relevant IAM role, remove any local references to credentials.
+
+sudo yum -y install jq gettext bash-completion
+
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+
+export AWS_CLUSTER_NAME=eksworkshop-eksctlv10
+
+aws configure set default.region ${AWS_REGION}
+#aws configure set aws_output json
+
+sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+sudo curl --silent --location -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+sudo chmod +x /usr/local/bin/aws-iam-authenticator
+
+/usr/local/bin/kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+git clone https://github.com/brentley/ecsdemo-frontend.git
+git clone https://github.com/brentley/ecsdemo-nodejs.git
+git clone https://github.com/brentley/ecsdemo-crystal.git
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv -v /tmp/eksctl /usr/local/bin
+eksctl version
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+ssh-keygen -t rsa -q -P "" -f ~/.ssh/id_rsa-${AWS_CLUSTER_NAME}
+
+aws ec2 import-key-pair --key-name "${AWS_CLUSTER_NAME}" --public-key-material file://~/.ssh/id_rsa-${AWS_CLUSTER_NAME}.pub
+
+aws kms create-alias --alias-name alias/${AWS_CLUSTER_NAME} --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
+
+export MASTER_ARN=$(aws kms describe-key --key-id alias/${AWS_CLUSTER_NAME} --query KeyMetadata.Arn --output text)
+
+echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
+
+cat << EOF > ${AWS_CLUSTER_NAME}.yaml
+---
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: ${AWS_CLUSTER_NAME}
+  region: ${AWS_REGION}
+
+managedNodeGroups:
+- name: nodegroup
+  desiredCapacity: 3
+  iam:
+    withAddonPolicies:
+      albIngress: true
+
+secretsEncryption:
+  keyARN: ${MASTER_ARN}
+EOF
+
+eksctl create cluster -f ${AWS_CLUSTER_NAME}.yaml
+eksctl utils write-kubeconfig --cluster ${AWS_CLUSTER_NAME}
+
+# Create Windows Jump Box for accessing KubeFlow Dashboard
+export EKSWORKSHOP_VPC=$(eksctl get cluster ${AWS_CLUSTER_NAME} | grep vpc | awk  '{print $5}')
+export windows_SG=$(aws ec2 create-security-group --group-name windowsSg-${AWS_CLUSTER_NAME} --description "Windows Jump Server security group" --vpc-id ${EKSWORKSHOP_VPC} | jq '.GroupId' | tr -d '"')
+export EKSWORKSHOP_SUBNET=$(aws ec2 describe-subnets --filter "Name=tag:kubernetes.io/role/elb, Values=1" | jq '.Subnets[0].SubnetId' | tr -d '"')
+aws ec2 run-instances --image-id ami-07f3715a1f6dbb6d9 --count 1 --instance-type t2.large --key-name ${AWS_CLUSTER_NAME} --security-group-ids ${windows_SG} --subnet-id ${EKSWORKSHOP_SUBNET} --associate-public-ip-address
