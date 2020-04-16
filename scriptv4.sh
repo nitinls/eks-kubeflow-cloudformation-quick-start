413533fb91baca2a5dfdcd2b5d8c9a311e9dc586
Amit Kalawat <amitkalawat@tests-mbp.lan>
2020-04-15 23:43:56 -0400

    Commit

--- scriptv4.sh
+++ scriptv4.sh
@@ -2,7 +2,13 @@
 # Setup Cloud9 on our Jump Server
 
 #Intall Node.Js
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+wget https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh /home/ec2-user/install.sh
+
+export NVM_DIR=/usr/local/bin
+
+bash /home/ec2-user/install.sh
+
+mv /usr/bin/nvm.sh /usr/bin/nvm
 
 source ~/.bashrc
 
